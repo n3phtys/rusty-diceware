51db1a21fab1d9645d28cec5a1f04349f424634b
nephtys <christopher.kaag@gmail.com>
2017-08-02 21:47:44 +0200

    removed u8 limit in favor of u32 combined with streaming string directly to stdout

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ fn main() {
         None => "5",
     };
 
-    let n: u8 = n_str.parse().unwrap();
+    let n: u32 = n_str.parse().unwrap();
 
     //println!("n = {}", n);
 
@@ -34,26 +34,25 @@ fn main() {
 
     let mut rng = OsRng::new().unwrap();
     let word_list: Vec<&str> = list::parse_list(list::EN_LIST);
-    println!("{}", build_diceware_password(&mut rng, n, &word_list));
+    build_diceware_password(&mut rng, n, &word_list);
 }
 
-fn build_diceware_password(rng : &mut OsRng, n: u8, list: &Vec<&str>) -> String {
+fn build_diceware_password(rng : &mut OsRng, n: u32, list: &Vec<&str>) -> () {
     let mut v: Vec<usize> = Vec::new();
 
     for _ in 0..n {
         v.push(dice::from_array_to_index(dice::roll_four_dice(rng)));
     }
 
-    let mut x: String = "".to_string();
+
 
     for (i, item) in v.iter().enumerate() {
         if i > 0usize {
-            x = x + "*";
+            print!("*");
         }
         let idx:usize = *item;
         let r: &str = list[idx];
-        x = x + r;
+        print!("{}", r);
     }
 
-    return x;
 }
